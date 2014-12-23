187f5e72eb7e735e2b6fd2a0b91d4c92dce585b0
Jens <jsproede@oev.de>
2014-12-23 08:06:06 +0100

    initial commit

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,22 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Jens Sproede (OEV) on 22.12.14.
+//  Copyright (c) 2014 OEV. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var name = "";
+    var breed = "";
+    var age = 0;
+    
+    var image = UIImage(named:"")
+    
+    func getName() -> String {
+        return name;
+    }
+}
--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var breedLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var myTiger = Tiger()
+        myTiger.name = "Tigger"
+        myTiger.breed = "Bengal"
+        myTiger.age = 3
+        myTiger.image = UIImage(named: "BengalTiger.jpg")
+        
+        var secondTiger = Tiger()
+        secondTiger.name = "Tigress"
+        secondTiger.breed = "Indochinese Tiger"
+        secondTiger.age = 2
+        secondTiger.image = UIImage(named: "IndochineseTiger.jpg")
+        
+        println("My tigers name ist \(myTiger.getName()), its age is \(myTiger.age) \(myTiger.breed) \(myTiger.image)")
+        
+        myImageView.image = myTiger.image
+        nameLabel.text = myTiger.getName()
+        ageLabel.text = "\(myTiger.age)"
+        breedLabel.text = myTiger.breed
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +46,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    @IBAction func nextBarButtonItemPressed(sender: UIBarButtonItem) {
+        
+    }
 }
 
